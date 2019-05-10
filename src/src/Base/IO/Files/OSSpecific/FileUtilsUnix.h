a843646d75d2f68c0f821cc0d71d089987005d74
Scorbutics <brice.bulgarelli@outlook.com>
2019-05-10 23:36:16 +0200

    feature: initialization

---
+++ src/src/Base/IO/Files/OSSpecific/FileUtilsUnix.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "../SkaConstants.h"
+#if defined(SKA_PLATFORM_LINUX)
+#include <string>
+
+namespace ska {
+	class FileUtilsUnix {
+	private:
+		FileUtilsUnix();
+
+	public:
+		~FileUtilsUnix();
+		static std::string getCurrentDirectory();
+		static std::string getExecutablePath();
+		static void createDirectory(const std::string& directoryName);
+	};
+}
+#endif
---
+++ test/src/main.cpp
@@ -0,0 +1,18 @@
+#define DOCTEST_CONFIG_IMPLEMENT
+#include <doctest.h>
+
+int main() {
+    doctest::Context context;
+    auto resultCode = 0;
+
+	//context.setOption("test-case", "[Interpreter Script]");
+    //context.setOption("order-by", "name");
+    //context.setOption("success", "true");
+    resultCode = context.run();
+
+    if (context.shouldExit() || resultCode != 0) {
+        exit(-1);
+    }
+}
+
+
