18c13d1bdb440bf4dac7db2fb6eae3c05d96a27e
ebbu6050 <brice.bulgarelli-ext@pole-emploi.fr>
2019-05-17 16:04:24 +0200

    test : fix for a bad parameter passing in cmd line when executing unit test

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -1,11 +1,10 @@
 #define DOCTEST_CONFIG_IMPLEMENT
 #include <doctest.h>
 
-int main() {
-    doctest::Context context;
+int main(int argv, char* argv[]) {
+    doctest::Context context = doctest::Context{argc, argv};
     auto resultCode = 0;
 
-	//context.setOption("test-case", "[Interpreter Script]");
     //context.setOption("order-by", "name");
     //context.setOption("success", "true");
     resultCode = context.run();
